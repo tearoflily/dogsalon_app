428360b29b027054e8bf32130405cea1deb15fd7
tearoflily <tearoflily1019@gmail.com>
2021-04-13 23:27:31 +0900

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   get 'home', to: 'home#index'
 
-  # namespace :api, format: 'json' do
-  #   namespace :v1 do
-  #     resources :memos, only: [:index, :create]
-  #   end
-  # end
+  namespace :api, format: 'json' do
+    namespace :v1 do
+      resources :memos, only: [:index, :create]
+    end
+  end
 end
