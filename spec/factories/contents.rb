86fb92e348c2e2b71f95401d4e47e143614115c1
tearoflily <tearoflily1019@gmail.com>
2021-04-15 23:13:49 +0900

    add auth to contents

---
+++ spec/factories/contents.rb
@@ -0,0 +1,6 @@
+# FactoryBot.define do
+#   factory :content do
+#     title { 'テストタイトル' }
+#     detail { 'テストコンテンツの明細です。' }
+#   end
+# end
---
+++ spec/support/javascript_driver.rb
@@ -0,0 +1,2 @@
+# During the cache time, Webdrivers won't check to update Chrome.
+Webdrivers.cache_time = 1.month.to_i
