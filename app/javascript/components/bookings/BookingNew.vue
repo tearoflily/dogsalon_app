e3dbb46acc55a003800df6b246416ec7a1051d09
Saki <tearoflily1019@gmail.com>
2021-05-03 08:16:53 +0900

    0503-予約新規作成

--- app/javascript/components/bookings/BookingNew.vue
+++ app/javascript/components/bookings/BookingNew.vue
@@ -1,90 +1,107 @@
 <template>
-  <v-app>
-    <v-row justify="center">
-      <!-- <BookingCalendar /> -->
-      <v-form @submit.prevent="createBooking">
-          <v-card width="400" tile light>
-              <v-card-title>予約新規登録画面</v-card-title>
-              <v-card-text>
-                <div v-if="errors.length != 0">
-                  <ul v-for="e in errors" :key="e">
-                    <li><font color="red">{{ e }}</font></li>
-                  </ul>
-                </div>
+  <div class="row">
+    <div class="col-md-6 offset-md-3 mb-5">
+      <div class="row">
+        <div :class="navigationCss(1)">お客様情報</div>
+        <div :class="navigationCss(2)">配送情報</div>
+        <div :class="navigationCss(3)">クーポン情報</div>
+      </div>
+    </div>
 
-    
+    <div class="col-md-4 offset-md-4" v-if="page==1">
+      <h1>お客様情報</h1>
+      <div class="form-group">
+      <label>名前</label>
+        <booking-on-calender v-model="params.name"></booking-on-calender>
+      </div>
+      <div class="form-group">
+        <label>かな</label>
+        <input class="form-control" type="text" v-model="params.name_kana">
+        <p>{{ params.name_kana }}</p>
+      </div>
+    </div>
 
-                <div>
-                  <v-text-field v-model="booking.start_date_time" 
-                    label="開始日時" outlined clearable></v-text-field>
-                  <v-text-field v-model="booking.end_date_time" 
-                    label="終了日時" outlined clearable></v-text-field>
-                  <v-text-field v-model="booking.booking_shop_comment" 
-                    label="店舗コメント" outlined clearable></v-text-field>
-                  <v-text-field v-model="booking.customer_id" 
-                    label="カスタマーID" outlined clearable></v-text-field>
-                  <v-text-field v-model="booking.pet_id" 
-                    label="ペットID" outlined clearable></v-text-field>
-                  <v-text-field v-model="booking.menu_id" 
-                    label="メニューID(複数可能)" outlined clearable></v-text-field>
-                </div>
-                
-              </v-card-text>
-              <v-catd-actions>
-                <v-btn color="primary" block type="submit">送信</v-btn>
-              </v-catd-actions>
-          </v-card>
-      </v-form>
-    </v-row>
-  </v-app>
+    <div class="col-md-4 offset-md-4" v-if="page==2">
+      <h1>配送情報</h1>
+      <div class="form-group">
+        <label>住所</label>
+        <input class="form-control" type="text" v-model="params.address">
+        <p>{{ params.address }}</p>
+      </div>
+      <div class="form-group">
+        <label>配達時間</label>
+        <select class="form-control"  v-model="params.delivery_time">
+          <option value="1">9〜10時</option>
+          <option value="2">10〜11時</option>
+          <option value="3">11〜12時</option>
+          <option value="4">13〜14時</option>
+          <option value="5">15〜16時</option>
+        </select>
+        <p>{{ params.delivery_time }}</p>
+      </div>
+    </div>
+
+    <div class="col-md-4 offset-md-4" v-if="page==3">
+      
+      <h1>本予約に関するコメント欄</h1>
+      <div class="form-group">
+      <label>コメント</label>
+        <booking-on-comment v-model="params.address"></booking-on-comment>
+      </div>
+    </div>
+
+
+    <div class="col-md-4 offset-md-4 pt-5">
+      <button class="btn btn-secondary float-right"
+      type="button" v-if="page<3" @click="next">次へ</button>
+      <button class="btn btn-secondary float-right"
+      type="button" v-if="page==3" @click="submit">送信する</button>
+      <button class="btn btn-secondary float-right"
+      type="button" v-if="page>1" @click="prev">戻る</button>
+    </div>
+
+  </div>
 </template>
 
 <script>
-import axios from 'axios';
+import BookingOnCalender from './BookingOnCalender.vue';
+import BookingOnComment from './BookingOnComment.vue';
 
-// import BookingCalendar from './BookingCalendar';
 
 export default {
-  components: {
-
-    // BookingCalendar
-
-  },
-  data: function () {
-    return {
-      booking: {
-        start_date_time: '',
-        end_date_time: '',
-        booking_shop_comment: '',
-        customer_id: '',
-        pet_id: '',
-        menu_id: ''
+    components: { BookingOnCalender, BookingOnComment },
+    data: function () {
+      return {
+        page: 1,
+        params: {
+          name: '名前を入力してください',
+          name_kana: 'かなを入力してください',
+          address: '住所を入力してください',
+          delivery_time: '時間帯を入力してください',
+          coupon_code: 'コードを入力してください'
+        },
+      }
+    },
+    methods: {
+      next() {
+        this.page++;
       },
-      errors: ''
+      prev() {
+        this.page--;
+      },
+      submit() {
+        // const url = 'http://localhost:3000/';
+        // axios.post(url, this.params)
+        // .then((response) => {
+
+        // });
+      },
+      navigationCss(targetPage) {
+        if(this.page === targetPage) {
+          return ['col-4', 'text-center', 'text-white', 'bg-info', 'p-2'];
+        }
+        return ['col-4', 'text-center', 'text-secondary', 'p-2'];
+      }
     }
-  },
-
-  
-  methods: {
-    createBooking: function() {
-      axios
-        .post('/api/v1/bookings/create', this.booking)
-        .then(response => {
-          let e = responese.data;
-          this.$router.push({ name: 'BookingDetailPage', params: { id: e.id} });
-        })
-        .catch(error => {
-          console.error(error);
-          if (error.response.data && error.response.data.errors) {
-            this.errors = error.response.data.errors;
-          }
-
-        });
-    }
-  }
-
-
-
-};
-
+  };
 </script>
---
+++ app/javascript/components/bookings/BookingOnCalender.vue
@@ -0,0 +1,44 @@
+<template>
+<div>
+  <input class="form-control" type="text" v-model="inputedValue">
+  <p>{{ value }}</p>
+
+<!-- <v-calendar :attributes="attrs" ></v-calendar>    -->
+
+</div>
+</template>
+
+
+<script>
+export default {
+  props:{
+    value: {
+      type: String
+    },
+  },
+  // data: { 
+  //   attrs: [
+  //       {
+  //         key: 'today',
+  //         highlight: {
+  //           backgroundColor: '#ff8080',
+  //         },
+  //         dates: new Date(),
+  //         popover: {
+  //           label: 'メッセージを表示できます',
+  //         },
+  //       }
+  //   ],
+  // },
+  computed: {
+    inputedValue: {
+      get() {
+        return this.value;
+      },
+      set(newValue) {
+        this.$emit("input", newValue);
+      },
+    },
+  },
+};
+</script>
